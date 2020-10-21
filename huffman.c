d713a1a128cfcf1681c97567415239e4b515177f
潘奕平 <panyiping@panyipings-MacBook-Pro.local>
2020-10-21 17:19:52 +0800

    --

--- huffman.c
+++ huffman.c
@@ -7,7 +7,7 @@
 #define INVALID_BIT_READ -1
 #define INVALID_BIT_WRITE -1
 
-unsigned char png_header[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+unsigned char signature[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
 
 int encode(const char *in_file, const char *out_file){
 	FILE *fin, *fout;
@@ -15,21 +15,19 @@ int encode(const char *in_file, const char *out_file){
         	perror("Failed to open input file");
         	return 1;
     	}
-    	if ((fout = fopen(out_file, "wb")) == NULL) {
-        	perror("Failed to open output file");
-        	fclose(fin);
-        	return 1;
-    	}
+	if ((fout = fopen(out_file, "wb")) == NULL) {
+		perror("Failed to open output file");
+		fclose(fin);
+		return 1;
+	}
 		
 	unsigned char head[8];
 	fread(head, sizeof(head), 1, fin);
-	if (memcmp(png_header, head, 8)){
+	if (memcmp(signature, head, 8)){
 		perror("Input file is not PNG format\n");
-		return 1;
+		return 1;	/* bad signature */
 	}
 	
-
-
 	return 0;
 }
 int decode(const char *in_file, const char *out_file){
