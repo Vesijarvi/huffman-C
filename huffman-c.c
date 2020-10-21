9ff95da6ea10fc704658ef12efe610d58ae820ae
潘奕平 <panyiping@panyipings-MacBook-Pro.local>
2020-10-21 12:53:56 +0800

    Impliment Check PNG

--- huffman-c.c
+++ huffman-c.c
@@ -5,6 +5,15 @@ int main(int argc, char *argv[]){
 	// argc[1]: myfile.png
 	
 	FILE *f = fopen(argv[1],"r");
+	unsigned char png_head[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+	unsigned char head[8];
+	fread(head, sizeof(head), 1, f);
+
+	// if the file is PNG
+	if (!memcmp(signature, head, 8))
+	{
+    		//...
+	}
 	
 
 	fclose(f);
