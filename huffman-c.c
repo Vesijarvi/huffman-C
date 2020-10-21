4c11c4d0c09a916ffd00d9ccb003f1e5eedbf098
潘奕平 <panyiping@panyipings-MacBook-Pro.local>
2020-10-21 12:48:13 +0800

    Impliment open file

--- huffman-c.c
+++ huffman-c.c
@@ -1,5 +1,12 @@
 #include<stdlib.h>
-int main(int argc, char *argc[]){
+int main(int argc, char *argv[]){
+	// argc: counter of arguments
+	// argv[0]: myprogram
+	// argc[1]: myfile.png
 	
+	FILE *f = fopen(argv[1],"r");
+	
+
+	fclose(f);
 	return 0;
 }
